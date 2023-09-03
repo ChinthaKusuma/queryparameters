b38e3ed8bb3df82a48830952db0e5572bd2083fe
kusuma <chinthakusuma25@gmail.com>
2023-09-03 13:26:06 +0000

    all APIS

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(express.json());
 const status1 = (requestQuery) => {
   return requestQuery.status != undefined;
 };
+
 const priority1 = (requestQuery) => {
   return requestQuery.priority != undefined;
 };
@@ -65,13 +66,54 @@ const initiliazeDbAndServer = async () => {
       response.send(dbResponse2);
     });
     app.post("/todos/", async (request, response) => {
-      const todoDetails = request.query;
-      const { todo, priority, status } = todoDetails;
-      const query3 = `insert into todo(todo,priority,status)
-      values('${todo}','${priority}','${status}');`;
+      const todoDetails = request.body;
+      const { id, todo, priority, status } = todoDetails;
+      const query3 = `insert into todo(id,todo,priority,status)
+      values(${id},'${todo}','${priority}','${status}');`;
 
       response.send("Todo Successfully Added");
       const dbResponse3 = await db.run(query3);
+    });
+    app.put("/todos/:todoId/", async (request, response) => {
+      const { todoId } = request.params;
+      let updated1 = "";
+      const requestBody = request.body;
+
+      switch (true) {
+        case requestBody.status !== undefined:
+          updated1 = "Status";
+          break;
+
+        case requestBody.priority !== undefined:
+          updated1 = "Priority";
+          break;
+
+        case requestBody.todo !== undefined:
+          updated1 = "Todo";
+          break;
+      }
+      const query44 = `select * from todo
+        where id=${todoId};`;
+      const dbResponse44 = await db.get(query44);
+      const {
+        todo = dbResponse44.todo,
+
+        priority = dbResponse44.priority,
+        status = dbResponse44.status,
+      } = request.body;
+      const query4 = `update todo set
+        
+        todo='${todo}',
+        priority='${priority}',status='${status}' where id=${todoId};`;
+
+      const dbResponse4 = await db.run(query4);
+      response.send(`${updated1} Updated`);
+    });
+    app.delete("/todos/:todoId/", async (request, response) => {
+      const { todoId } = request.params;
+      const query5 = `delete from todo where id=${todoId};`;
+      const dbResponse5 = await db.run(query5);
+      response.send("Todo Deleted");
     });
   } catch (e) {
     console.log("Db error");
